aff7e746950057861718ce10c1653d6627eea3e9
shamim <shamimhossainpro753159@yahoo.com>
2016-06-28 17:08:46 +0600

    Course Details section completed

--- app/src/main/java/com/shamsapp/shamscorner/com/pocketuni_forum/course_registration/CourseRegistration.java
+++ app/src/main/java/com/shamsapp/shamscorner/com/pocketuni_forum/course_registration/CourseRegistration.java
@@ -1,8 +1,12 @@
 package com.shamsapp.shamscorner.com.pocketuni_forum.course_registration;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.LinearLayout;
 
 import com.shamsapp.shamscorner.com.pocketuni_forum.R;
 
@@ -10,11 +14,27 @@ import com.shamsapp.shamscorner.com.pocketuni_forum.R;
  * Created by shamim on 24-Jun-16.
  */
 public class CourseRegistration extends AppCompatActivity {
+
+    public static final String LOGINPREF = "loginpref" ;
+    SharedPreferences sharedpreferences;
+    String username = "";
+
+    LinearLayout regMainLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.course_reg_panel);
 
+        // set the sharedpreferences
+        sharedpreferences = getSharedPreferences(LOGINPREF, Context.MODE_PRIVATE);
 
+        //get the username from the sharedpreference
+        username = sharedpreferences.getString("USERNAME", "");
+
+        regMainLayout = (LinearLayout)findViewById(R.id.reg_main_container);
+
+        // call the course details class
+        new RegistrationDetailsView(this, regMainLayout).execute(username);
     }
 }
--- app/src/main/java/com/shamsapp/shamscorner/com/pocketuni_forum/course_registration/RegistrationDetailsView.java
+++ app/src/main/java/com/shamsapp/shamscorner/com/pocketuni_forum/course_registration/RegistrationDetailsView.java
@@ -4,8 +4,14 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.shamsapp.shamscorner.com.pocketuni_forum.R;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -19,16 +25,14 @@ import java.net.URLEncoder;
  */
 public class RegistrationDetailsView extends AsyncTask<String, Void, String> {
 
-    private TextView tvError;
     private Context context;
-    public static final String LOGINPREF = "loginpref" ;
-    String username, password;
-    SharedPreferences sharedpreferences;
+    String rollNo;
     ProgressDialog progressDialog;
+    LinearLayout regMainContainer;
 
-    public RegistrationDetailsView(Context context, TextView tvError){
+    public RegistrationDetailsView(Context context, LinearLayout regMainContainer){
         this.context = context;
-        this.tvError = tvError;
+        this.regMainContainer = regMainContainer;
     }
 
     protected void onPreExecute(){
@@ -38,12 +42,10 @@ public class RegistrationDetailsView extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String... arg) {
         try{
-            username = arg[0];
-            password = arg[1];
+            rollNo = arg[0];
 
-            String link = "http://shamscorner001.site88.net/Uni_Forumb69c5929474a3779df762577b7cce8eb/UniForum/mlogin.php";
-            String data = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8");
-            data += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+            String link = "http://shamscorner001.site88.net/Uni_Forumb69c5929474a3779df762577b7cce8eb/UniForum/mCourse_reg_details.php";
+            String data = URLEncoder.encode("roll_no", "UTF-8") + "=" + URLEncoder.encode(rollNo, "UTF-8");
 
             URL url = new URL(link);
             URLConnection conn = url.openConnection();
@@ -74,6 +76,28 @@ public class RegistrationDetailsView extends AsyncTask<String, Void, String> {
     @Override
     protected void onPostExecute(String result){
         String[] value = result.split("//");
+        Log.d("Length", ""+value.length);
+
+        int count = 0, loop = 0;
+        while(loop < (value.length/3)){
+            // inflate a xml file in a non activity class
+            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.registration_details, null);
+
+            TextView courseId = (TextView)layout.findViewById(R.id.tv_course_id);
+            TextView title = (TextView)layout.findViewById(R.id.tv_course_title);
+            TextView credit = (TextView)layout.findViewById(R.id.tv_course_credit);
+
+            courseId.setText(""+value[count]);
+            count++;
+            title.setText(""+value[count]);
+            count++;
+            credit.setText(""+value[count]);
+            count++;
+
+            regMainContainer.addView(layout);
+            loop++;
+        }
 
         progressDialog.dismiss();
     }
