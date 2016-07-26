0b4511b06b82b0d846fb56cc0ceb280be52c22d0
shamim <shamimhossainpro753159@yahoo.com>
2016-07-26 07:31:25 +0600

    ct marks show dialog for students part 1

--- app/src/main/java/com/shamsapp/shamscorner/com/pocketuni_forum/class_test/ClassTestResultInput.java
+++ app/src/main/java/com/shamsapp/shamscorner/com/pocketuni_forum/class_test/ClassTestResultInput.java
@@ -1,10 +1,15 @@
 package com.shamsapp.shamscorner.com.pocketuni_forum.class_test;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Spinner;
 
 import com.shamsapp.shamscorner.com.pocketuni_forum.R;
@@ -18,7 +23,8 @@ public class ClassTestResultInput extends AppCompatActivity {
     private Spinner semester, courseId;
     public static final String LOGINPREF = "loginpref" ;
     SharedPreferences sharedpreferences;
-    String username_text;
+    private String username_text, semesterText, courseIdText;
+    private LayoutInflater inflater;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,4 +45,32 @@ public class ClassTestResultInput extends AppCompatActivity {
         new SpinnerItemAddition(this, semester).execute(username_text, "distinct semester", "course_takes", "roll_no");
         new SpinnerItemAddition(this, courseId).execute(username_text, "distinct course_id", "course_takes", "roll_no");
     }
+
+    public void ctGetStartedStudent(View view) {
+        semesterText = semester.getSelectedItem().toString();
+        courseIdText = courseId.getSelectedItem().toString();
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        // Get the layout inflater
+        inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater = LayoutInflater.from(ClassTestResultInput.this);
+
+        alertDialogBuilder.setView(inflater.inflate(R.layout.ct_marks_dialog_show, null))
+                .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
 }
+/*
+
+select ct_no, marks from ct_marks where roll_no = '123086' and semester = 1 and course_id = 'CSE 100'
+
+ */
